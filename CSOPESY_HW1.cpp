071eba46e63b5cec96235841058f0396426c7f6a
Kiel Cipriaso <kielcipri@gmail.com>
2024-09-10 17:42:08 +0800

    initializing repo

---
+++ CSOPESY_HW1.cpp
@@ -0,0 +1,59 @@
+//CSOPESY Homework #2
+//S11 - Cipriaso, James Kielson S.
+
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+#include <algorithm>
+#include <stdlib.h>
+using namespace std;
+
+void menu(int mode, string command){
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    wcout << L" ██████╗███████╗ ██████╗ ██████╗ ███████╗███████╗██╗   ██╗\n";
+    wcout << L"██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔════╝╚██╗ ██╔╝\n";
+    wcout << L"██║     ███████╗██║   ██║██████╔╝█████╗  ███████╗ ╚████╔╝ \n";
+    wcout << L"██║     ╚════██║██║   ██║██╔═══╝ ██╔══╝  ╚════██║  ╚██╔╝  \n";
+    wcout << L"╚██████╗███████║╚██████╔╝██║     ███████╗███████║   ██║   \n";
+    wcout << L" ╚═════╝╚══════╝ ╚═════╝ ╚═╝     ╚══════╝╚══════╝   ╚═╝   \n" << endl;
+    _setmode(_fileno(stdout), _O_TEXT);
+    cout << "\033[32;1mWelcome to the CSOPESY Terminal!\033[0m" << endl;
+    cout << "\033[33mType 'exit' to quit, Type 'clear to clear screen\033[0m" << endl;
+    if(mode==1)
+        cout << "'" << command << "'" << " is an invalid command!" << endl;
+    else if(mode==0)
+        cout << "'" << command << "'" << " command recognized. Doing something...." << endl;
+}
+
+int commands(string option){
+    string commandArray[] = {"initialize", "screen", "scheduler-test", "scheduler-stop", "report-util"};
+    string commandOutput = " command recognized. Doing something....";
+
+    if(option=="clear")
+        system("cls");
+    else if(option=="exit"){
+        cout << "Goodbye!";
+        exit(0);
+    }
+    else if(find(commandArray, commandArray+5, option) != commandArray+5)
+        return 0;
+    else
+        return 1;
+    return -1;
+}
+
+int main(){
+    int displayMode = 2;
+    string displayCommand = " ";
+    while(true){
+        system("cls");
+        menu(displayMode, displayCommand);
+        string option;
+        cout << "Enter command: ";
+        cin >> option;
+        cout << "\n";
+        displayMode = commands(option);
+        displayCommand = option;
+        cout << "\n";
+    }
+}
