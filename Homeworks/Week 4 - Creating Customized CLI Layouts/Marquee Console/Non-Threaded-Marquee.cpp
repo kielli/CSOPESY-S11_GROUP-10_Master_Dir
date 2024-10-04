eabd628ec8d0e797137a43ad42f927fefce1f30f
Kiel Cipriaso <kielcipri@gmail.com>
2024-10-04 22:05:47 +0800

    Changed Non-Threaded code

--- Homeworks/Week 4 - Creating Customized CLI Layouts/Marquee Console/Non-Threaded-Marquee.cpp
+++ Homeworks/Week 4 - Creating Customized CLI Layouts/Marquee Console/Non-Threaded-Marquee.cpp
@@ -9,15 +9,12 @@ using namespace std;
 //Constant Variables for storing inputs as well as displays
 vector<char> keyInputs;
 string storeInput, userInput;
-
 const int refreshRate = 2000; //Change refresh rate as necessary, higher refresh rate gives slower marquee
-
 const string marqueeText = "Hello World in marquee!";
 const char *headerText =
-                        "*********************************\n"
-                        "* Displaying a marquee console! *\n"
-                        "*********************************";
-
+    "**************************************\n"
+    "*    Displaying a marquee console!   *\n"
+    "**************************************";
 const string inputFieldText = "Enter a command for MARQUEE_CONSOLE: ";
 
 //Function for converting vector<char> to string
@@ -30,55 +27,38 @@ class MarqueeConsole {
     int coordX = 0, coordY = 2, length = 150, width = 30; //Change length and width as necessary
     bool borderX = false, borderY = false;
 
-    //Sets the console cursor to a given row (x) and column (y)
-    void setCursorPosition(int x, int y) {
+    void setCursorPosition(int x, int y) { //Sets the console cursor to a given row (x) and column (y)
         COORD coord = { static_cast<SHORT>(x), static_cast<SHORT>(y) };
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
     }
 
-    //Updates the coords of the marquee by checking if it has approached the border of the screen, adjusts the direction if screen is approached
-    void updateMarqueeCoords() {
+    void updateMarqueeCoords() { //Updates the coords of the marquee by checking if it has approached the border of the screen, adjusts the direction if screen is approached
         coordX += borderX ? -1 : 1;
         coordY += borderY ? -1 : 1;
-
-        if (coordX >= length - marqueeText.size()) 
-            borderX = true;
-        else if (coordX < 1) 
-            borderX = false;
-        if (coordY >= width - 1) 
-            borderY = true;
-        else if (coordY < 3) 
-            borderY = false;
+        if (coordX >= length - marqueeText.size() - 10) borderX = true;
+        else if (coordX < 1) borderX = false;
+        if (coordY >= width - 1) borderY = true;
+        else if (coordY < 3) borderY = false;
     }
 
-    //Prints the header of the marquee console
-    void printHeader() {
+    void printHeader() { //Prints the header of the marquee console
         cout << headerText; 
     }
-
-    //Prints the actual marquee text
-    void printMarquee() { 
+    void printMarquee() { //Prints the actual marquee text
         setCursorPosition(coordX, coordY); cout << setfill(' ') << marqueeText; 
     }
-
-    //Prints the input field
-    void printInputField() {
+    void printInputField() { //Prints the input field
         setCursorPosition(0, width+1); cout << inputFieldText; 
     }
-
-    //Prints the character typed by the user by means of keyboard polling
-    void printStoredInput() {
+    void printStoredInput() { //Prints the character typed by the user by means of keyboard polling
         cout << storeInput; setCursorPosition(inputFieldText.size() + keyInputs.size(), width+1); 
     }
-
-    //Prints confirmation of command previously entered
-    void printCommandConfirm() {
+    void printCommandConfirm() { //Prints confirmation of command previously entered
         if (!userInput.empty()) cout << "\nCommand Processed in MARQUEE_CONSOLE: " << userInput; setCursorPosition(inputFieldText.size() + keyInputs.size(), width+1); 
     }
 
 public:
-    //Step-by-step process of the marquee in order to show different frames
-    void updateConsole() {
+    void updateConsole() { //Step-by-step process of the marquee in order to show different frames
         system("cls");
         printHeader();
         printMarquee();
@@ -89,24 +69,15 @@ public:
     }
 };
 
-// Helper function to check if Caps Lock is ON
-bool isCapsLockActive() {
-    return (GetKeyState(VK_CAPITAL) & 0x0001) != 0;
-}
-
-//Acts as the interface for Event Handler
-class IKeyboardEvent { 
+class IKeyboardEvent { //Acts as the interface for Event Handler
 public:
     virtual void OnKeyDown(char key) = 0;
     virtual void OnKeyUp(char key) = 0;
 };
 
-//Triggers certain actions depending on the button pressed
-class KeyboardEventHandler : public IKeyboardEvent {
+class KeyboardEventHandler : public IKeyboardEvent { //Triggers certain actions depending on the button pressed
 public:
-
-    //Actions done when the keyboard button is pressed down
-    void OnKeyDown(char key) override {
+    void OnKeyDown(char key) override { //Actions done when the keyboard button is pressed down
         if (GetAsyncKeyState(VK_BACK) & 0x8000 && !keyInputs.empty()) { //Manages Backspace button event
             keyInputs.pop_back();
         } else if (GetAsyncKeyState(VK_RETURN) & 0x8000) { //Manages Enter button event
@@ -118,20 +89,6 @@ public:
         } else if (GetAsyncKeyState(VK_SHIFT) & 0x8000 || GetAsyncKeyState(VK_CAPITAL) & 0x8000) { //Manages Shift button event
             keyInputs.push_back(key);
         }
-         else { // Handle normal key press, with capitalization checks
-            bool shiftPressed = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
-            bool capsLock = isCapsLockActive();
-
-            if (isalpha(key)) { // Check if the key is an alphabet character
-                if ((shiftPressed && !capsLock) || (!shiftPressed && capsLock)) {
-                    key = toupper(key); // Make uppercase
-                }
-                else {
-                    key = tolower(key); // Make lowercase
-                }
-            }
-            keyInputs.push_back(key);
-        }
         storeInput = vectorToString(keyInputs); //Temporarily stores the string value stored in the keyInputs vector
     }
     void OnKeyUp(char key) override { //Actions done when the keyboard button has been let go
@@ -140,8 +97,7 @@ public:
     }
 };
 
-//Manages keyboard polling
-void PollKeyboard(IKeyboardEvent& keyboardEvent, MarqueeConsole& mconsole) {
+void PollKeyboard(IKeyboardEvent& keyboardEvent, MarqueeConsole& mconsole) { //Manages keyboard polling
     int i = 0;
     while (true) {
         if (i++ == refreshRate) { //Manages the refresh rate, frame will only update when the value of i reaches the refresh rate value
