71f4398926c0b73f5e8048009c20242d91dacb79
HooDue(Letty) <127380830+HooDue@users.noreply.github.com>
2024-10-29 19:29:19 +0800

    Add files via upload
    new changes1 : working scheduler test and scheduler stop
    - scheduler test creates dummy procecess
    - when key is triggered will let user input command
    - when user input scheduler -stop the scheduler test stops creating dummy procecess
    
    new changes 2: changed classes
    - a screen is a process, screen inherits properties of a process
    - instruction will be a new class to separate it from others.
    
    new changes3: mainProgram accepts initialization
    - prob is its manual, need to change after
    
    will move on to the fcfs scheduler : )

---
+++ merged_version5/CreatedScreen.h
@@ -0,0 +1,28 @@
+#ifndef CREATEDSCREEN_H
+#define CREATEDSCREEN_H
+
+#include "Screen.h"
+#include "ScreenManager.h"
+#include <string>
+
+using namespace std;
+
+class CreatedScreen : public Screen {
+
+private:
+    string screenName;
+    string creationTimestamp;
+    ScreenManager* screenManager;  // Pointer to ScreenManager for switching screens
+
+public:
+    CreatedScreen(const string& name, ScreenManager* manager);  // Pass ScreenManager in constructor
+
+    void display() override;
+    void displayHeader();
+    void handleCommand(const string& command) override;
+
+    const string& getScreenName() const;
+    string getTimestamp() const;
+};
+
+#endif
---
+++ merged_version5/KeyboardEventHandler.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+#include <conio.h>
+#include <vector>
+
+#ifndef KEYBOARDEVENTHANDLER_H
+#define KEYBOARDEVENTHANDLER_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class IKeyboardEvent {
+public:
+    virtual void OnKeyDown(char key) = 0;
+    virtual void OnKeyUp(char key) = 0;
+};
+
+class KeyboardEventHandler : public IKeyboardEvent {
+public:
+    void OnKeyDown(char key) override;
+    void OnKeyUp(char key) override;
+    string getUserInput();
+
+private:
+    vector<char> keyInputs;
+    string userInput;
+    string storeInput;
+    string vectorToString(const std::vector<char>& input);
+};
+
+//void PollKeyboard(IKeyboardEvent& keyboardEvent);
+
+#endif // KEYBOARDEVENTHANDLER_H
