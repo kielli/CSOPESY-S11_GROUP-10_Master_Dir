a55193f76e603c24fc88a3fc288b841a42b9d28b
Kiel Cipriaso <kielcipri@gmail.com>
2024-11-30 18:51:47 +0800

    Paging Allocator implemented

--- Memory/PagingAllocator.h
+++ Memory/PagingAllocator.h
@@ -1,7 +1,23 @@
 #pragma once
-
 #include "IMemoryAllocator.h"
+#include <unordered_map>
+#include <vector>
+#include <mutex>
 
 class PagingAllocator : public IMemoryAllocator {
+    public:
+        PagingAllocator(size_t maxMemSize);
 
+        void* allocate(Process* process) override;
+        void deallocate(Process* process) override;
+
+    private:
+        size_t maxMemSize;
+        size_t numFrames;
+        std::unordered_map<size_t, size_t> frameMap;
+        std::vector<size_t> freeFrameList;
+        std::mutex mtx;
+
+        size_t allocateFrames(size_t numFrames, size_t proccessId);
+        void deallocateFrames(std::vector<size_t> framesToBeFreed);
 };
